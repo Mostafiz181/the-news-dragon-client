b7ada35e21b7c1a4b5d325aa2bfd0cc80669ab2c
mostafizurrahman <dev.mostafizurrahman@gmail.com>
2023-04-29 11:15:30 -0700

    added header part and moment js

--- src/shared/Header/Header.jsx
+++ src/shared/Header/Header.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
+import logo from "../../assets/logo.png";
+import moment from 'moment';
 
 const Header = () => {
-    return (
-        <div>
-            <h2>this is header</h2>
-        </div>
-    );
+  return (
+    <div>
+      <div className="text-center">
+        <img src={logo} alt="" />
+        <h4>Journalism Without Fear or Favour</h4>
+        <p>{moment().format("dddd, MMMM D YYYY, ")}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
