cd440db0361ea2188c1309a21523cdda2800f52d
mostafizurrahman <dev.mostafizurrahman@gmail.com>
2023-04-29 10:23:52 -0700

    environment setup for dragon project done

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDsiUCna4zonm1bL1utXzmEG0selQu8WF4",
+  authDomain: "the-news-dragon-client-41563.firebaseapp.com",
+  projectId: "the-news-dragon-client-41563",
+  storageBucket: "the-news-dragon-client-41563.appspot.com",
+  messagingSenderId: "798694053203",
+  appId: "1:798694053203:web:24d832c800de528d82bda6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
